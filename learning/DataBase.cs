36d14a3961354a109debb8da17473ededaf560e9
VaniVl <moskvitin.228@gmail.com>
2023-10-28 14:56:52 +0300

    Добавьте файлы проекта.

---
+++ learning/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    internal class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=141.8.192.151;port=3306;username=f0878880_VaniVl;password=228338ljv;database=f0878880_massager");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ learning/LoginForm.cs
@@ -0,0 +1,90 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learning
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            this.label2.Text = "QQ!";
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            this.label2.Text = "X";
+        }
+
+        Point lastPoint;
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            String loginU = textBox1.Text;
+            String passwordU = textBox2.Text;
+
+            DataBase db = new DataBase();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @lU AND `password` = @pU", db.getConnection());
+            command.Parameters.Add("@lU", MySqlDbType.VarChar).Value = loginU;
+            command.Parameters.Add("@pU", MySqlDbType.VarChar).Value = passwordU;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count > 0)
+                MessageBox.Show("Welcome back!");
+            else
+                MessageBox.Show("wrong login or password");
+        }
+    }
+}
